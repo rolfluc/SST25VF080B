997b9d75621aad16a88d3425e1f952d0b1873528
Rolfes <lucasatarc@gmail.com>
2022-12-28 12:40:25 -0600

    WIP - Starting Driver for SST25V

---
+++ SST25VF080B.c
@@ -0,0 +1,141 @@
+#include "SST25VF080B.h"
+
+extern int SPI_Write(uint8_t* bytes, uint32_t len);
+extern int SPI_Read(uint8_t* bytes, uint32_t len);
+extern int SPI_Transfer(uint8_t* writeBytes, uint8_t* readBytes, uint32_t len);
+
+static inline void AdaptAddress(uint32_t address, uint8_t* target)
+{
+	target[0] = (uint8_t)(address >> 16);
+	target[1] = (uint8_t)(address >> 8);
+	target[2] = (uint8_t)(address);
+}
+
+SS25_Error SS25_Init()
+{
+	//TODO no initialization necessary?
+	return ERR_Okay;
+}
+
+SS25_Error SS25_Read(uint32_t address, uint8_t* bytes, uint32_t numBytes)
+{
+	
+}
+
+SS25_Error SS25_Write(uint32_t address, uint8_t* bytes, uint32_t numBytes)
+{
+	
+}
+
+SS25_Error SS25_Erase(uint32_t address, SS25_EraseSize sze)
+{
+	uint8_t writeBuffer[4] = { 0,0,0,0 };
+	uint8_t writeSize = 0;
+	switch (sze)
+	{
+	case Size_4kb:
+		{
+			writeBuffer[0] = FOUR_KB_SECTOR_ERASE;
+			writeSize = sizeof(writeBuffer);
+			break;
+		}
+	case Size_32kb:
+		{
+			writeBuffer[0] = THIRTY_TWO_KB_BLOCK_ERASE;
+			writeSize = sizeof(writeBuffer);
+			break;
+		}
+	case Size_64kb:
+		{
+			writeBuffer[0] = SIXTY_FOUR_KB_BLOCK_ERASE;
+			writeSize = sizeof(writeBuffer);
+			break;
+		}
+	case Size_Chip:
+		{
+			writeBuffer[0] = CHIP_ERASE;
+			//Special case - only writes one byte as no address is necessary.
+			writeSize = 1;
+			break;
+		}
+	default:
+		return ERR_Fail;
+		break;
+	}
+	AdaptAddress(address, &writeBuffer[1]);
+	
+	uint32_t bytes = SPI_Write(writeBuffer, writeSize);
+	if (bytes == writeSize)
+	{
+		return ERR_Okay;
+	}
+	else
+	{
+		return ERR_Fail;
+	}
+}
+
+SS25_Error SS25_GetStatus(SS25_Status* status)
+{
+	SS25_Status stat;
+	
+}
+
+SS25_Error SS25_SetWriteEnable(bool doEnable)
+{
+	uint8_t writeBuffer = 0;
+	if (doEnable)
+	{
+		writeBuffer = (uint8_t)WREN;
+	}
+	else
+	{
+		writeBuffer = (uint8_t)WRDI;
+	}
+	uint32_t bytes = SPI_Write(&writeBuffer, sizeof(writeBuffer));
+	if (bytes == sizeof(writeBuffer))
+	{
+		return ERR_Okay;
+	}
+	else
+	{
+		return ERR_Fail;
+	}
+}
+
+SS25_Error SS25_SetSREnable(bool doEnable)
+{
+	uint8_t writeBuffer = 0;
+	if (doEnable)
+	{
+		writeBuffer = EWSR;
+	}
+	else
+	{
+		//TOD Not implemented yet.
+		return ERR_Fail;
+	}
+	uint32_t bytes = SPI_Write(&writeBuffer, sizeof(writeBuffer));
+	if (bytes == sizeof(writeBuffer))
+	{
+		return ERR_Okay;
+	}
+	else
+	{
+		return ERR_Fail;
+	}
+}
+
+SS25_Error SS25_WriteSR(uint8_t sr)
+{
+	uint8_t writeBuffer[2] = { WRSR, sr };
+	uint32_t bytes = SPI_Write(writeBuffer, sizeof(writeBuffer));
+	if (bytes == sizeof(writeBuffer))
+	{
+		return ERR_Okay;
+	}
+	else
+	{
+		return ERR_Fail;
+	}
+}
---
+++ SST25VF080B.h
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#include <stdbool.h>
+typedef enum {
+	READ                      = 0x03,
+	HIGH_SPEED_READ           = 0x0B, 
+	FOUR_KB_SECTOR_ERASE      = 0x20,
+	THIRTY_TWO_KB_BLOCK_ERASE = 0x52,
+	SIXTY_FOUR_KB_BLOCK_ERASE = 0xD8,
+	CHIP_ERASE                = 0x60,
+	BYTE_PROGRAM              = 0x02,
+	AAI_WORD_PROGRAM          = 0xAD,
+	RDSR                      = 0x05,
+	EWSR                      = 0x50,
+	WRSR                      = 0x01,
+	WREN                      = 0x06,
+	WRDI                      = 0x04,
+	RDID                      = 0x90,
+	JEDECID                   = 0x9F,
+	EBSY                      = 0x70,
+	DBSY                      = 0x80,
+} SS25_Commands;
+
+typedef enum {
+	ERR_Okay = 0,
+	ERR_Fail,
+} SS25_Error;
+
+typedef enum {
+	Status_Okay,
+} SS25_Status;
+
+typedef enum {
+	Size_4kb,
+	Size_32kb,
+	Size_64kb,
+	Size_Chip,
+} SS25_EraseSize;
+
+SS25_Error SS25_Init();
+SS25_Error SS25_Read(uint32_t address, uint8_t* bytes, uint32_t numBytes);
+SS25_Error SS25_Write(uint32_t address, uint8_t* bytes, uint32_t numBytes);
+SS25_Error SS25_Erase(uint32_t address, SS25_EraseSize sze);
+SS25_Error SS25_GetStatus(SS25_Status* status);
+SS25_Error SS25_SetWriteEnable(bool doEnable);
+SS25_Error SS25_SetSREnable(bool doEnable);
+SS25_Error SS25_WriteSR(uint8_t sr);
